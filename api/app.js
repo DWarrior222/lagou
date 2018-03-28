b5835690e1a4d6522334f4620ebb281709e9be2e
guanshuaijie <1845543324@qq.com>
2018-03-28 10:29:49 +0800

    服务端解决跨域

--- api/app.js
+++ api/app.js
@@ -16,7 +16,14 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.use('/users', users);
 
 app.use('/users', users);
